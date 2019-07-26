8cbc65fef32fa0eb512580fe303a4def05acc89e
andreassosilo <andreas.sosilo@gmail.com>
2019-07-26 14:09:50 +0700

    Modify 6.js with Math.pow()

--- phase0/week6/simulationLiveCode/6.js
+++ phase0/week6/simulationLiveCode/6.js
@@ -17,18 +17,22 @@ function parseNumber (equation) {
   equation = equation.toString()
   // Base case
   if (equation.length === 1 && equation === '0') {
-    return equation
+    return ''
   } else if (equation.length === 1) {
     return equation
   }
   // Get the multiplier
   let multiplier = Math.pow(10, equation.length - 1)
-
   // Return the message
-  return equation[0] * Number(multiplier) + '+' + parseNumber(Number(equation.slice(1)))
+  // If the rest elements in the string are 0s, return the value itself
+  if (!Number(equation.slice(1))) {
+    return equation[0] * multiplier + ''
+  } // If not, continue the recursion
+  return equation[0] * multiplier + '+' + parseNumber(Number(equation.slice(1)))
 }
 
 console.log(parseNumber(3333)) // 3000+300+30+3
 console.log(parseNumber(90)) // 90
 console.log(parseNumber(2333)) // 2000+300+30+3
 console.log(parseNumber(5050)) // 5000+50
+console.log(parseNumber(550000)) // 5 5 000
