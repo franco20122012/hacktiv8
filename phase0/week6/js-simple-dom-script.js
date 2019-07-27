a5473f351bc0a94562bb09f4cb787b0a7b5064a8
andreassosilo <andreas.sosilo@gmail.com>
2019-07-27 21:13:39 +0700

    Add js-simple-dom-script.js

---
+++ phase0/week6/js-simple-dom-script.js
@@ -0,0 +1,11 @@
+let pageTitleElement = document.getElementById('page-title')
+// Menyeleksi DOM berdasarkan Id element dan menampungnya ke dalam variabel.
+// Isinya merupakan object HTML element
+
+let pageBoxElements = document.getElementsByClassName('page-box')
+// Menyeleksi DOM berdasarkan nama class element dan menampungnya ke dalam variabel.
+// Isinya merupakan array dari object HTML element, walau <h1> hanya ada 1.
+
+let pageHeadings = document.getElementsByTagName('h1')
+// Menyeleksi DOM berdasarkan tag <h1> dan menampungnya ke dalam variabel.
+// Isinya merupakan array dari object HTML element
