d438e63a72d2db88a3f359633c9ee00d5d4bded4
andreassosilo <andreas.sosilo@gmail.com>
2019-07-27 21:50:32 +0700

    Modify js-simple-dom-script.js

--- phase0/week6/js-simple-dom-script.js
+++ phase0/week6/js-simple-dom-script.js
@@ -9,3 +9,19 @@ let pageBoxElements = document.getElementsByClassName('page-box')
 let pageHeadings = document.getElementsByTagName('h1')
 // Menyeleksi DOM berdasarkan tag <h1> dan menampungnya ke dalam variabel.
 // Isinya merupakan array dari object HTML element
+
+let pageTitleElementsContent = pageTitleElement.innerHTML
+console.log('isi <div id="page-title"> :' + pageTitleElementsContent)
+// isi <div id="page-title"> adalah Sample Page Title
+
+// Meloop array pageBoxElements
+for (let i = 0; i < pageBoxElements.length; i++) {
+  let currentPageBoxElement = pageBoxElements[i]
+  let currentPageBoxElementContent = currentPageBoxElement.innerHTML
+  console.log('isi <div class="page-box"> index ke ' + i + ': ' + currentPageBoxElementContent)
+}
+
+// Mengubah isi pageTitleElement
+pageTitleElement.innerHTML = '<h2>Updated Content of Page Title Element</h2>'
+let newPageTitleElementContent = pageTitleElement.innerHTML
+console.log('isi baru dari <div id="page-title"> :' + newPageTitleElementContent)
