32cf54b411d7d13e60373d1ac9a2de93778b5370
andreassosilo <andreas.sosilo@gmail.com>
2019-07-29 11:33:52 +0700

    Refactor 002.js final live code batch33

--- phase0/week6/finalLiveCodeBatch33/002.js
+++ phase0/week6/finalLiveCodeBatch33/002.js
@@ -11,6 +11,8 @@ Kamu mengikuti lomba marathon, dengan rute lintasan tertentu dan stamina tertent
 
 [EXAMPLE]
 input lintasan: 'XXXXXXOOO-XXOOXXXXXOO-XXXXO'
+['XXXX', 'XX', 'OO', O-XXOOXXXXXOO-XXXXO']
+[-1,   3]
 
 'X' adalah jalan datar yang akan mengkonsumsi 1 stamina dengan maskimal repetisi 'X' 4 kali  ('XXXX')
 'O' adalah jalan menanjak yang akan mengkonsumsi 1 stamina dengan maksimal repetisi 'O' 2 kali ('OO')
@@ -32,39 +34,45 @@ function marathon (track, stamina) {
   // Check if there are no inputs, return message
   if (!track || !stamina) return `Wrong input!`
   // Create variables for running distance and repetition
-  let distance = 0
   let repetition = 0
-  let changeTrack = false
-  // Use for-loop to check the score
+  let changeTrack = true
+
+  // Do looping to split the track into trackArr
   for (let i = 0; i < track.length; i++) {
-    // Check repetition first
-    if ()
-    if (repetition < 1) {
+    // Check if repetition is reached or changeTrack is true
+    if (changeTrack) {
+      repetition = 0
+    }
+
+    // Check repetition and stamina first
+    if (repetition < 1 && track[i] !== '-') {
       if (stamina > 0) {
         stamina--
         repetition = 4
       } else {
-        return `Stamina habis`
+        return `Selamat anda telah menempuh jarak ${i} km`
       }
     }
-    // Check the track
-    if (track[i] === 'X' && changeTrack === true) {
-      repetition -= 1
-    } else if (track[i] === 'X' && changeTrack === false) {
 
-    }
-    else if (track[i] === 'O') {
+    // Check the track
+    if (track[i].toUpperCase() === 'X') {
+      repetition -= 1
+    } else if (track[i].toUpperCase() === 'O') {
       repetition -= 2
     } else if (track[i] === '-') {
       stamina += 2
     }
-  }
-  // Check who is the winner
-  return `Selamat anda telah menem`
-}
 
-function checkRepetition(stamina, repetition) {
-  return repetition
+    // Check the change track
+    if (i < track.length - 1 && track[i] === track[i + 1]) {
+      changeTrack = false
+    } else {
+      changeTrack = true
+    }
+    // console.log(`current track: ${track[i]}, distance: ${i + 1}, stamina: ${stamina}, repetition: ${repetition}, changeTrack: ${changeTrack}`)
+  }
+  // Return message if finishing the marathon
+  return `Selamat anda telah menempuh garis finish`
 }
 
 console.log(marathon('XXXXXXOOO-XXOOXXXXXOO-XXXXO', 5)) // Selamat anda telah menempuh jarak 18 km
