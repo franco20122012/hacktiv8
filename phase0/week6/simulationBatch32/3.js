fd64fdd6c0fae10c77633d50e3b28812a93a40ce
andreassosilo <andreas.sosilo@gmail.com>
2019-07-30 14:25:12 +0700

    Refactor 3.js simulationBatch32

--- phase0/week6/simulationBatch32/3.js
+++ phase0/week6/simulationBatch32/3.js
@@ -8,7 +8,6 @@ Diberikan sebuah function yang menerima parameter array bilangan bulat. Dimana
 function ini berfungsi untuk mengembalikan jumlah maksimum dari dua buah nilai
 yang dijumlahkan didalam array tersebut
 
-
 [EXAMPLE]
 - Bila array adalah [-2, 1, 3, -4, 5], maka nilai maksimum adalah 8 karena dua nilai yang
 dapat menghasilkan nilai maksimum adalah 3 + 5
@@ -19,21 +18,21 @@ dapat menghasilkan nilai maksimum adalah 3 + 5
 3. dilarang menggunakan map, filter, reduce, apply
 */
 
-function maxSubsetSum(arr) {
-  for(var i = arr.length-1 ; i >= 0 ; i--){
-    for(var j = 0 ; j < i ; j++){
-      if(arr[j] < arr[j+1]){
-        var temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;
+function maxSubsetSum (arr) {
+  for (var i = arr.length - 1; i >= 0; i--) {
+    for (var j = 0; j < i; j++) {
+      if (arr[j] < arr[j + 1]) {
+        var temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
       }
     }
   }
-  return arr[0] + arr[1];
+  return arr[0] + arr[1]
 }
-  
-console.log(maxSubsetSum([-2, 1, 3, -4, 5])); //8
-console.log(maxSubsetSum([-1, 2, 5, 7])); //12
+
+console.log(maxSubsetSum([-2, 1, 3, -4, 5])) // 8
+console.log(maxSubsetSum([-1, 2, 5, 7])) // 12
 console.log(
   maxSubsetSum([
     943,
@@ -48,6 +47,5 @@ console.log(
     485947,
     474262
   ])
-);
+)
 // 9574938
-  
