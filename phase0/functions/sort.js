b36f44019b17f0dd10362e877ce4765b0ee32447
andreassosilo <andreas.sosilo@gmail.com>
2019-07-31 06:33:48 +0700

    Add manual sort function

---
+++ phase0/functions/sort.js
@@ -0,0 +1,13 @@
+// Create function to sort using bubble sort algorithm
+function sorting (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+  return arr
+}
