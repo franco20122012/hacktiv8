d72ce5a84438405b7b50284f03dd029f637f523e
andreassosilo <andreas.sosilo@gmail.com>
2019-07-31 08:52:09 +0700

    Modify datareducer 006.js finalLiveCodeBatch33

--- phase0/week6/finalLiveCodeBatch33/006.js
+++ phase0/week6/finalLiveCodeBatch33/006.js
@@ -21,13 +21,39 @@ output: c,d,e,f,g,h,i,j
 */
 
 function dataReducer (data) {
-  // BASE CASE
-  if (data.length > 2) {
-    return (data[0].charCodeAt() + 1 === data[1].charCodeAt()) ? dataReducer(data.slice(1)) : data[1] + ',' + dataReducer(data[0] + data.slice(2))
-  } else {
-    return (data[0].charCodeAt() + 1 === data[1].charCodeAt()) ? '' : data[1]
+  let result = ''
+  let newData = ''
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  let index
+  let recursive = null
+
+  for (let i = 0; i < alphabet.length; i++) {
+    if (alphabet[i] === data[0]) index = i
   }
+
+  if (data[1] === alphabet[index + 1]) newData = data.substr(1)
+  else {
+    result += data[1]
+    newData = data[0] + data.substr(2)
+  }
+
+  if (data.length === 2) return result
+  recursive = dataReducer(newData)
+
+  if (recursive && result.length) {
+    return result + ',' + recursive
+  }
+  return result + recursive
 }
+
+// function dataReducer (data) {
+//   // BASE CASE
+//   if (data.length > 2) {
+//     return (data[0].charCodeAt() + 1 === data[1].charCodeAt()) ? dataReducer(data.slice(1)) : data[1] + ',' + dataReducer(data[0] + data.slice(2))
+//   } else {
+//     return (data[0].charCodeAt() + 1 === data[1].charCodeAt()) ? '' : data[1]
+//   }
+// }
 
 console.log(dataReducer('abcdxefgh5wi')) // 'x,5,w'
 console.log(dataReducer('opqrstu')) // ''
